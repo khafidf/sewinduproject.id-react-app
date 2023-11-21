614cd163253927094f696556f00432e84206eb94
khafidf <khfd027@gmail.com>
2023-11-21 14:13:13 +0700

    Fix about us

--- src/pages/client/home/AboutUs.jsx
+++ src/pages/client/home/AboutUs.jsx
@@ -5,21 +5,22 @@ export const AboutUs = () => {
 	return (
 		<div className="w-full">
 			<div className="container mx-auto">
-				<div className="relative px-2 mx-auto py-4 h-[504px]">
+				<div className="relative px-2 mx-auto w-[360px] sm:w-[420px] lg:w-[528px] py-4 h-[504px]">
 					<div className="absolute w-[200px] rounded-lg h-[120px] shadow-sm bg-gray-900">
-						<h1 className="absolute text-lg font-bold text-blue-gray-50 left-8 top-10 md:text-xl lg:text-2xl">
+						<h1 className="absolute text-base font-bold text-blue-gray-50 left-8 top-12 md:text-lg lg:text-xl">
 							~ About Us
 						</h1>
 					</div>
-					<div className="absolute w-[270px] top-24 p-4 end-2 rounded-lg h-[224px] bg-gray-100 shadow-md">
-						<p className="p-2 text-base text-blue-gray-900">
-							Sewindu Project, is your go-to destination for heartfelt
-							photography sessions. We specialize in capturing moments with
-							genuine emotion, turning them into everlasting memories.
+					<div className="absolute w-[270px] top-24 p-4 end-2 rounded-lg h-[224px] lg:w-[360px] md:w-[320px] bg-gray-100 shadow-md">
+						<p className="px-2 py-8 text-sm md:text-base md:py-6 text-blue-gray-900">
+							Sewindu Project is a leading provider of photography and
+							videography services, offering a wide range of services including
+							weddings, pre-weddings, yearbooks, couple sessions, and
+							graduations.
 						</p>
 					</div>
-					<div className="absolute w-[240px] bottom-24 p-4 rounded-lg h-[108px] bg-gray-900 shadow-md">
-						<p className="absolute p-2 text-base top-8 text-blue-gray-50">
+					<div className="absolute w-[240px] lg:w-[254px] bottom-24 p-4 rounded-lg h-[108px] bg-gray-900 shadow-md">
+						<p className="absolute p-2 text-sm md:text-base top-8 text-blue-gray-50">
 							Based in Mojokerto
 						</p>
 					</div>
