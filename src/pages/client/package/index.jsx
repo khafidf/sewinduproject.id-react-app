98d1b95f4892972dc8019f0b6a7404af8ea18dc4
khafidf <khfd027@gmail.com>
2023-11-21 16:42:11 +0700

    Add Hero to packages

--- src/pages/client/package/index.jsx
+++ src/pages/client/package/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Section } from "./Section";
+import { Hero } from "./Hero";
 
 export default function PackagePage() {
 	return (
 		<>
+			<Hero />
 			<Section />
 		</>
 	);
