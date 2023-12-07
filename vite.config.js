ad948ad2926e27b5fad4011ed24d37315efa341a
khafidf <khfd027@gmail.com>
2023-12-07 15:37:57 +0700

    new config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import compress from "vite-plugin-compress";
+import { compression } from "vite-plugin-compression2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		compress({
-			verbose: true,
-			disable: false,
-			threshold: 10240,
-			algorithm: "gzip",
-			ext: ".gz",
-		}),
-	],
+	plugins: [react(), compression()],
 });
