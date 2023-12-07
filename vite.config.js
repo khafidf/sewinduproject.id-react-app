ac98394aebff1b8bee247ff86cf873f5584b1408
khafidf <khfd027@gmail.com>
2023-12-07 15:34:08 +0700

    new config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { viteCompression } from "vite-plugin-compress";
+import compress from "vite-plugin-compress";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
-		viteCompression({
+		compress({
 			verbose: true,
 			disable: false,
 			threshold: 10240,
